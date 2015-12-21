f628d4fcf38b600307ded94bb330e0f4aa471e63
Kurt Dillen <kurt.dillen@dls-belgium.com>
2015-12-21 11:11:31 +0100

    * Add /usr/local/bin/zabbix-db-setup.sh to start_postgres

--- container-files/start_postgres.sh
+++ container-files/start_postgres.sh
@@ -5,3 +5,5 @@ exec su postgres -c "/usr/pgsql-9.4/bin/postgres -D /var/lib/pgsql/9.4/data"
 
 # Update default postgres user password
 #exec su postgres -c "psql -c \"alter user postgres password 'password'\";"
+
+exec /usr/local/bin/zabbix-db-setup.sh
